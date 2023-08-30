5ced312fb69a1a09dcee94a31a30a024fc47afe2
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-08-30 16:29:23 -0600

    added functions to store scores in local storage and update a 'high scores' page with new scores

--- assets/script.js
+++ assets/script.js
@@ -8,46 +8,50 @@ var startSection = document.getElementById("start");
 var quizSection = document.getElementById("quiz");
 var resultSection = document.getElementById("result");
 var initials = document.getElementById("initials");
-var submitButton = document.getElementById("#submit-button");
-var inputElement = document.getElementById("#initials");
+var submitButton = document.getElementById("submit-button");
+var inputElement = document.querySelector("#initials");
 var score = document.getElementById("score");
+var highScoreEl = document.getElementById("high-score-item");
+var highScoreOl = document.getElementById("highscore-list");
 
 //define variables and objects for questions
-
 
 //array of objects
 
 var questions = [
     {
-        question: "question 1",
-        choices:["words", "words", "words", "words"],
-        correctAnswer: 0
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 2,
     },
-
     {
-        question: "question 2",
-        choices:["words", "words", "words", "words"],
-        correctAnswer: 3
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 2,
     },
-
     {
-        question: "question 3",
-        choices:["words", "words", "words", "words"],
-        correctAnswer: 1
+      title: 'Arrays in JavaScript can be used to store ____.',
+      choices: [
+        'numbers and strings',
+        'other arrays',
+        'booleans',
+        'all of the above',
+      ],
+      answer: 3,
     },
-
     {
-        question: "question 4",
-        choices:["words", "words", "words", "words"],
-        correctAnswer:4
+      title:
+        'String values must be enclosed within ____ when being assigned to variables.',
+      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+      answer: 2,
     },
-
     {
-        question: "question 5",
-        choices:["words", "words", "words", "words"],
-        correctAnswer:2
+      title:
+        'A very useful tool used during development and debugging for printing content to the debugger is:',
+      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      answer: 3,
     },
-];
+  ];
 
 
 var currentQuestionIndex = 0;
@@ -60,13 +64,13 @@ var timeInterval;
 
 //start timer/quiz
 function startQuiz() {
-
+//hide the start page and show the quiz section
 startSection.classList.add("hide");
 quizSection.classList.remove("hide");
-
+//use timeInterval to set up timer that decrements by one second with added text of seconds remaining
     timeInterval = setInterval(function () {
       timeScore--; timerDisplay.textContent = timeScore + " seconds remaining";
-  
+  //if time is 0 --> call gameOver function
       if (timeScore === 0 ) {
         timerDisplay.textContent = timeScore
         gameOver();
@@ -100,12 +104,16 @@ quizSection.classList.remove("hide");
 
 function displayQuestion() {
     currentQ = questions[currentQuestionIndex];
-    questionElement.textContent = currentQ.question;
+    questionElement.textContent = currentQ.title;
 
+    //create a loop that iterates through the questions and create a click event that runs the answerSel function
 choicesElement.textContent = " ";
  for (let i = 0; i < currentQ.choices.length; i++) {
     const choice = currentQ.choices[i];
+
+    //target li element in css
     var li = document.createElement("li");
+    li.classList.add("button");
     li.setAttribute("id",i);
     li.textContent=choice;
     li.addEventListener("click", answerSel);
@@ -113,13 +121,15 @@ choicesElement.textContent = " ";
  }
 }
 
+//function for the click event when an answer is chosen
 function answerSel(event) {
+
+//how do we capture the actual choice the user selected so then we can check that choice against that question's answer string?
     var userChoice = event.target.id;
     console.log(userChoice);
     //check if answer is correct -- > check user choice with correct (if statement)
     if (userChoice != currentQ.correctAnswer){
-        timeScore -= 15
-        alert("Try Again!");
+        timeScore -= 15;
     };
     currentQuestionIndex++;
     if (currentQuestionIndex < questions.length && timeScore > 0) {
@@ -130,7 +140,6 @@ function answerSel(event) {
     
 }
 
-var result = document.getElementById("result");
 
 function gameOver() {
     quizSection.classList.add("hide");
@@ -156,21 +165,53 @@ function storeScore(event) {
     return;
   }
 
-  //store score and initials in an object
-  var highScoreItem = {
-    initials: inputElement.value,
-    score: time,
-  };
+    //store score and initials in an object
+    var highScoreItem = {
+        score: timeScore,
+        initials: inputElement.value,
+      };
+
+  updateScore(highScoreItem);
+  //hide the result, display the highScoreItem
+  resultSection.classList.add("hide"); 
+highScoreEl.classList.remove("hide");
+var highScoreArray = getHighScore();
+highScoreArray.sort((b, a) => a.score - b.score); 
+
+highScoreOl.textContent = " ";
+for (let i = 0; i < highScoreArray.length; i++) {
+    const scoreObj = highScoreArray[i];
+    
+    var li = document.createElement("li");
+    li.textContent= `initials: ${scoreObj.initials} | score:${scoreObj.score}`;
+    highScoreOl.append(li);
+    
 }
+};
 
 
-updateScore(highScoreItem);
 
-//hide the question card, display the leaderboardcard
-hideResult();
-highScoreItem.remove("hidden");
 
-renderLeaderboard();
+    function getHighScore() {
+        var arr = localStorage.getItem("highScoreArray");
+        if (arr) {
+            return JSON.parse(arr);
+        } else {
+            return [];
+        }
+      
+      }
+
+//updates the high scores stored in local storage
+function updateScore(highScoreItem) {
+    var highScoreArray = getHighScore();
+    //append new highscore item to scoreboard array
+    highScoreArray.push(highScoreItem);
+    localStorage.setItem("highScoreArray", JSON.stringify(highScoreArray));
+  };
+
+
+
 
 //add event listener for start button
 startButton.addEventListener("click", startQuiz);
@@ -178,12 +219,3 @@ startButton.addEventListener("click", startQuiz);
 //store user initials and score when submit button is clicked
 submitButton.addEventListener("click", storeScore);
 
-
-//store score in local storage and present it back into another form -->
-//scores:
-//initials:
-//key value --> high scores --> array of objects that look the same in their key structure
-//loop for rendering each high score
-
-
-//separate pages for quiz and high scores
